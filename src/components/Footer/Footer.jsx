d265dfb680368dd59fc3faed5ebfac3a1dd95bea
masonleonhart <mason.leonhart@gmail.com>
2021-03-19 10:35:25 -0500

    modified file structure removed uneccessary components and created the ones we need

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -7,7 +7,33 @@ import './Footer.css';
 // or even care what the redux state is, so it doesn't need 'connect()'
 
 function Footer() {
-  return <footer>&copy; Prime Digital Academy</footer>;
+  return (
+    <footer>
+      Powered By{" "}
+      <a href="https://www.emergingacademy.org/">Emerging Digital Academy</a>
+      <br />
+      <div id="linkedin-footer-links-wrapper">
+        <a
+          href="https://www.linkedin.com/in/bryce-barsness-b70068207/"
+          target="_blank"
+        >
+          linkedin.com/in/bryce-barsness-b70068207
+        </a>
+        <a href="https://www.linkedin.com/in/masonleonhart/" target="_blank">
+          linkedin.com/in/masonleonhart
+        </a>
+        <a
+          href="https://www.linkedin.com/in/quinnjohnsonfargo/"
+          target="_blank"
+        >
+          linkedin.com/in/quinnjohnsonfargo
+        </a>
+        <a href="https://www.linkedin.com/in/treolsonfargo/" target="_blank">
+          linkedin.com/in/treolsonfargo
+        </a>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;

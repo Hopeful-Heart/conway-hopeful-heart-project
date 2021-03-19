01db9607daf85b5c861509a070aff413946ff689
Quinn Johnson <johnson.fargo87@gmail.com>
2021-03-19 15:58:31 -0500

    added event get and post

--- server/routes/events.router.js
+++ server/routes/events.router.js
@@ -1,18 +1,35 @@
 const express = require('express');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
 const router = express.Router();
-
-/**
+/*
  * GET route template
  */
 router.get('/', (req, res) => {
+  const queryText = `
+    SELECT * FROM "events";
+  `
+  pool.query(queryText).then(result => {
+    res.send(result.rows)
+  }).catch(err => {
+    res.send(500)
+  })
   // GET route code here
 });
-
-/**
+/*
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post('/', rejectUnauthenticated, (req, res) => {
+  const queryText = `
+    INSERT INTO "events" ( "user_id", "name", "date", "location", "description", "type" )
+    VALUES ($1, $2, $3, $4, $5, $6);
+  `
+  pool.query(queryText, [req.user.id, req.body.name, req.body.date, req.body.location, req.body.description, req.body.type]).then(result => {
+    res.send(result.rows)
+  }).catch(err => {
+    res.send(500)
+    console.log(err)
+  })
   // POST route code here
 });
 

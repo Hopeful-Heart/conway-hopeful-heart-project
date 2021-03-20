4879d0bc2024cdbc270e12ffbd664dd815bbc939
Quinn Johnson <johnson.fargo87@gmail.com>
2021-03-19 22:53:22 -0500

    added get event by id and upcoming events in next ten days

--- server/routes/events.router.js
+++ server/routes/events.router.js
@@ -2,9 +2,8 @@ const express = require('express');
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const pool = require('../modules/pool');
 const router = express.Router();
-/*
- * GET route template
- */
+
+//GET for all events
 router.get('/', (req, res) => {
   const queryText = `
     SELECT * FROM "events";
@@ -14,11 +13,37 @@ router.get('/', (req, res) => {
   }).catch(err => {
     res.send(500)
   })
-  // GET route code here
 });
-/*
- * POST route template
- */
+//GET for event by id
+router.get("/:id", rejectUnauthenticated, (req, res) => {
+  const queryText = `
+  SELECT * FROM "events" WHERE "id"=$1;
+  `
+  pool
+    .query(queryText, [req.params.id])
+    .then(result => {
+      res.send(result.rows)
+    })
+    .catch((err) => {
+      res.sendStatus(500);
+    });
+});
+
+//GET for the next 10 events
+router.get('/', (req, res) => {
+  const queryText = `
+  SELECT * FROM "events"
+  WHERE "date" <= NOW() + interval '10 days';
+  `
+  //sets params to look out the next 10 days
+  pool.query(queryText).then(result => {
+    res.send(result.rows)
+  }).catch(err => {
+    res.send(500)
+  })
+});
+
+//POST to add an event
 router.post('/', rejectUnauthenticated, (req, res) => {
   const queryText = `
     INSERT INTO "events" ( "user_id", "name", "date", "location", "description", "type", "link" )
@@ -30,9 +55,9 @@ router.post('/', rejectUnauthenticated, (req, res) => {
     res.send(500)
     console.log(err)
   })
-  // POST route code here
 });
 
+//DELETE for specific event
 router.delete('/:id', rejectUnauthenticated, (req,res) => {
   const queryText = ` 
     DELETE FROM "events" WHERE "id"=$1;

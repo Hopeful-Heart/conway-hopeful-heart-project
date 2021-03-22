f69baaae876de26fa500ef2872bb0eadce7beeac
Bryce Barsness <bryce5@gmail.com>
2021-03-22 09:10:48 -0500

    added journal Get

--- server/routes/journal.router.js
+++ server/routes/journal.router.js
@@ -1,18 +1,31 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // GET route code here
+});
+
+router.get("/:id", (req, res) => {
+  // GET jobs by id for details page
+  id = req.params.id;
+  const queryText = `SELECT * FROM "journal" WHERE "user_id" = ${id}`;
+  pool
+    .query(queryText)
+    .then((result) => res.send(result.rows))
+    .catch((err) => {
+      console.log(`error in journal GET by user_id with`, err);
+      res.sendStatus(500);
+    });
 });
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // POST route code here
 });
 

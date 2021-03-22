c9a50d569df711c95db94cf444198cf95a1b379b
Bryce Barsness <bryce5@gmail.com>
2021-03-22 09:59:51 -0500

    added journal post request

--- server/routes/journal.router.js
+++ server/routes/journal.router.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
 const pool = require("../modules/pool");
 const router = express.Router();
 
@@ -25,8 +28,23 @@ router.get("/:id", (req, res) => {
 /**
  * POST route template
  */
-router.post("/", (req, res) => {
+router.post("/", rejectUnauthenticated, (req, res) => {
   // POST route code here
+  const queryText = `INSERT INTO "journal" ("user_id", "date", "content", "public")
+    VALUES ($1, $2, $3, $4)`;
+  console.log(req.body);
+  pool
+    .query(queryText, [
+      req.user.id,
+      req.body.date,
+      req.body.content,
+      req.body.public,
+    ])
+    .then((result) => res.send(result.rows))
+    .catch((err) => {
+      console.log(`error in Journal Post with`, err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;

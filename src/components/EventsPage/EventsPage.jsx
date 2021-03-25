888764234d893465e131a665e1f482521703f8bd
masonleonhart <mason.leonhart@gmail.com>
2021-03-25 11:12:01 -0500

    events popover good

--- src/components/EventsPage/EventsPage.jsx
+++ src/components/EventsPage/EventsPage.jsx
@@ -1,12 +1,14 @@
-import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid";
+import Calendar from "react-calendar";
 import { useSelector } from "react-redux";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import moment from "moment";
+
+import { Popover } from "@material-ui/core";
 
 import AddEventForm from "./AddEventForm";
 
 import "./EventsPage.css";
+import "react-calendar/dist/Calendar.css";
 
 // This is one of our simplest components
 // It doesn't have local state
@@ -15,25 +17,55 @@ import "./EventsPage.css";
 
 function EventsPage() {
   const events = useSelector((store) => store.events.approvedEventsListReducer);
-  const fullCalendarEvents = [];
+  const [anchorElement, setAnchorElement] = useState(null);
+  const [popoverEvent, setPopoverEvent] = useState({});
 
-  const createFullCalendarEvents = () => {
-    events.map((event) =>
-      fullCalendarEvents.push({
-        title: event.name,
-        date: moment(event.date).format("YYYY-MM-DD"),
-      })
-    );
-  };
-
-  events.length > 0 && createFullCalendarEvents();
+  const open = Boolean(anchorElement);
 
   return (
     <div className="container">
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
-        events={fullCalendarEvents}
+      <Popover
+        open={open}
+        anchorEl={anchorElement}
+        onClose={() => setAnchorElement(null)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "bottom",
+          horizontal: "center",
+        }}
+      >
+        <div id="popover-content">
+          <h3>{popoverEvent.name}</h3>
+          <p>Date: {moment(popoverEvent.date).format('MM-DD-YYYY')}</p>
+          <p>Location: {popoverEvent.location}</p>
+          <p>Event Type: {popoverEvent.type}</p>
+          <p>Description: {popoverEvent.description}</p>
+          <a href={popoverEvent.link}>Link to Event</a>
+        </div>
+      </Popover>
+      <Calendar
+        calendarType="US"
+        minDetail="month"
+        tileContent={({ activeStartDate, date, view }) =>
+          events.map((event) =>
+            view === "month" &&
+            moment(date).format("YYYY-MM-DD") ===
+              moment(event.date).format("YYYY-MM-DD") ? (
+              <p
+                key={event.id}
+                onClick={(e) => {
+                  setAnchorElement(e.target);
+                  setPopoverEvent(event);
+                }}
+              >
+                {event.name}
+              </p>
+            ) : null
+          )
+        }
       />
       <br />
       <br />
